5c52d38e63803a505178e48674f2da8992ade6d8
jiyoon0114 <greenpkh02@naver.com>
2026-06-22 00:16:41 +0900

    260622 로그인, 회원가입, 메인 페이지, authStore, Header.vue 변경 + 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Header from "./components/Header.vue";
+import { useAuthStore } from "./store/auth/useAuthStore.js";
+
+const authStore = useAuthStore();
 </script>
 
 <template>
-  <Header />
+  <Header v-if="authStore.authInitialized"/>
+  <router-view></router-view>
 </template>
